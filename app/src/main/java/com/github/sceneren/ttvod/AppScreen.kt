4c290cac69477cc9a03b66cbc027d2aa2cac3c6a
scene <376124750@qq.com>
2025-07-23 17:35:39 +0800

    统一到一个包

--- app/src/main/java/com/github/sceneren/ttvod/AppScreen.kt
+++ app/src/main/java/com/github/sceneren/ttvod/AppScreen.kt
@@ -10,13 +10,17 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -28,14 +32,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.LifecycleResumeEffect
+import com.bytedance.playerkit.player.PlayerEvent
+import com.bytedance.playerkit.player.event.InfoProgressUpdate
+import com.bytedance.playerkit.player.event.InfoSubtitleTextUpdate
 import com.bytedance.playerkit.player.playback.DisplayModeHelper
 import com.bytedance.playerkit.player.playback.DisplayView
 import com.bytedance.playerkit.player.playback.PlaybackController
 import com.bytedance.playerkit.player.playback.VideoView
 import com.bytedance.playerkit.player.source.MediaSource
+import com.bytedance.playerkit.player.volcengine.VolcConfig
+import com.bytedance.playerkit.player.volcengine.VolcDebugTools
 import com.bytedance.playerkit.player.volcengine.VolcScene
+import com.bytedance.playerkit.utils.event.Dispatcher
 import com.ss.ttvideoengine.Resolution
 import com.ss.ttvideoengine.SubInfoSimpleCallBack
 import com.ss.ttvideoengine.TTVideoEngine
@@ -57,14 +68,18 @@ import org.json.JSONObject
 const val VID = "v02d9bg10064d19n6kaljhtce8tgivj0"
 
 const val PLAY_AUTH_TOKEN =
-    "eyJHZXRQbGF5SW5mb1Rva2VuIjoiVmlkPXYwMmQ5YmcxMDA2NGQxOW42a2Fsamh0Y2U4dGdpdmowJlgtRXhwaXJlcz0zNjAwMCZBY3Rpb249R2V0UGxheUluZm8mVmVyc2lvbj0yMDIwLTA4LTAxJlgtRGF0ZT0yMDI1MDcxMFQwMjIxMzRaJlgtTm90U2lnbkJvZHk9JlgtQ3JlZGVudGlhbD1BS0xUT1dOaE5EWTVNalEwTWpGbE5HVmhNbUl6WkdZelpUYzFNRE5qWXpReU56SSUyRjIwMjUwNzEwJTJGY24tbm9ydGgtMSUyRnZvZCUyRnJlcXVlc3QmWC1BbGdvcml0aG09SE1BQy1TSEEyNTYmWC1TaWduZWRIZWFkZXJzPSZYLVNpZ25lZFF1ZXJpZXM9QWN0aW9uJTNCVmVyc2lvbiUzQlZpZCUzQlgtQWxnb3JpdGhtJTNCWC1DcmVkZW50aWFsJTNCWC1EYXRlJTNCWC1FeHBpcmVzJTNCWC1Ob3RTaWduQm9keSUzQlgtU2lnbmVkSGVhZGVycyUzQlgtU2lnbmVkUXVlcmllcyZYLVNpZ25hdHVyZT00MzNmMGM1NzU1MmRkY2ViNjYzZmQ4N2MzMjY4ZjU0MjA5YWM2Mjc3Y2MzNGI4ODI2OTUwYzE4Njg2MDYyZmEzIiwiVG9rZW5WZXJzaW9uIjoiVjIifQ=="
+    "eyJHZXRQbGF5SW5mb1Rva2VuIjoiVmlkPXYwMmQ5YmcxMDA2NGQxOW42a2Fsamh0Y2U4dGdpdmowJlgtRXhwaXJlcz0zNjAwMCZBY3Rpb249R2V0UGxheUluZm8mVmVyc2lvbj0yMDIwLTA4LTAxJlgtRGF0ZT0yMDI1MDcxMVQwMjMwMTVaJlgtTm90U2lnbkJvZHk9JlgtQ3JlZGVudGlhbD1BS0xUT1dOaE5EWTVNalEwTWpGbE5HVmhNbUl6WkdZelpUYzFNRE5qWXpReU56SSUyRjIwMjUwNzExJTJGY24tbm9ydGgtMSUyRnZvZCUyRnJlcXVlc3QmWC1BbGdvcml0aG09SE1BQy1TSEEyNTYmWC1TaWduZWRIZWFkZXJzPSZYLVNpZ25lZFF1ZXJpZXM9QWN0aW9uJTNCVmVyc2lvbiUzQlZpZCUzQlgtQWxnb3JpdGhtJTNCWC1DcmVkZW50aWFsJTNCWC1EYXRlJTNCWC1FeHBpcmVzJTNCWC1Ob3RTaWduQm9keSUzQlgtU2lnbmVkSGVhZGVycyUzQlgtU2lnbmVkUXVlcmllcyZYLVNpZ25hdHVyZT0xZGFmOGVlZWFiMWU5MGMzNTY3ZTRhMDk1YjFhMzcxODRmYzJjNjIzNWY2YmJlOGQyY2Q0ZDRlNGE4ODRlZTc4IiwiVG9rZW5WZXJzaW9uIjoiVjIifQ=="
 
 const val SUBTITLE_AUTH_TOKEN =
-    "eyJHZXRTdWJ0aXRsZUF1dGhUb2tlbiI6IlZpZD12MDJkOWJnMTAwNjRkMTluNmthbGpodGNlOHRnaXZqMCZTdGF0dXM9UHVibGlzaGVkJlgtRXhwaXJlcz0zNjAwMCZBY3Rpb249R2V0U3VidGl0bGVJbmZvTGlzdCZWZXJzaW9uPTIwMjAtMDgtMDEmWC1EYXRlPTIwMjUwNzEwVDAyMjEzNFomWC1Ob3RTaWduQm9keT0mWC1DcmVkZW50aWFsPUFLTFRPV05oTkRZNU1qUTBNakZsTkdWaE1tSXpaR1l6WlRjMU1ETmpZelF5TnpJJTJGMjAyNTA3MTAlMkZjbi1ub3J0aC0xJTJGdm9kJTJGcmVxdWVzdCZYLUFsZ29yaXRobT1ITUFDLVNIQTI1NiZYLVNpZ25lZEhlYWRlcnM9JlgtU2lnbmVkUXVlcmllcz1BY3Rpb24lM0JTdGF0dXMlM0JWZXJzaW9uJTNCVmlkJTNCWC1BbGdvcml0aG0lM0JYLUNyZWRlbnRpYWwlM0JYLURhdGUlM0JYLUV4cGlyZXMlM0JYLU5vdFNpZ25Cb2R5JTNCWC1TaWduZWRIZWFkZXJzJTNCWC1TaWduZWRRdWVyaWVzJlgtU2lnbmF0dXJlPTlkODY1MjBiODRiYjg4Njk5NWQ4Nzg4MWNiNDhjOTliNjU3OWQ2MWI3Mjg0MzRhNTljMTAwZjNmZTBjNWRhNTYifQ=="
+    "eyJHZXRTdWJ0aXRsZUF1dGhUb2tlbiI6IlZpZD12MDJkOWJnMTAwNjRkMTluNmthbGpodGNlOHRnaXZqMCZTdGF0dXM9UHVibGlzaGVkJlgtRXhwaXJlcz0zNjAwMCZBY3Rpb249R2V0U3VidGl0bGVJbmZvTGlzdCZWZXJzaW9uPTIwMjAtMDgtMDEmWC1EYXRlPTIwMjUwNzExVDAyMzAxNVomWC1Ob3RTaWduQm9keT0mWC1DcmVkZW50aWFsPUFLTFRPV05oTkRZNU1qUTBNakZsTkdWaE1tSXpaR1l6WlRjMU1ETmpZelF5TnpJJTJGMjAyNTA3MTElMkZjbi1ub3J0aC0xJTJGdm9kJTJGcmVxdWVzdCZYLUFsZ29yaXRobT1ITUFDLVNIQTI1NiZYLVNpZ25lZEhlYWRlcnM9JlgtU2lnbmVkUXVlcmllcz1BY3Rpb24lM0JTdGF0dXMlM0JWZXJzaW9uJTNCVmlkJTNCWC1BbGdvcml0aG0lM0JYLUNyZWRlbnRpYWwlM0JYLURhdGUlM0JYLUV4cGlyZXMlM0JYLU5vdFNpZ25Cb2R5JTNCWC1TaWduZWRIZWFkZXJzJTNCWC1TaWduZWRRdWVyaWVzJlgtU2lnbmF0dXJlPTdlOGNlODY1MWI2MzkzNjFkYjc5MDczYWZhZTA2Njg4MDM1Mzc4YTJlY2VjNTM0ZGRkNzc0ODlkNThkYThjZGMifQ=="
 
 @Composable
 fun AppScreen(modifier: Modifier, playbackController: PlaybackController) {
-    TTVideoView(modifier = modifier, playbackController = playbackController)
+    // TTVideoView(modifier = modifier, playbackController = playbackController)
+    Box(modifier = modifier) {
+
+        VideoList(playbackController = playbackController)
+    }
 }
 
 
@@ -307,7 +322,10 @@ internal fun createVideoSource(vid: String, playAuthToken: String): StrategySour
 private fun createVidMediaSource(): MediaSource {
     val mediaId = VID
     val playAuthToken = PLAY_AUTH_TOKEN
-    return MediaSource.createIdSource(mediaId, playAuthToken)
+    return MediaSource.createIdSource(mediaId, playAuthToken).apply {
+        subtitleAuthToken = SUBTITLE_AUTH_TOKEN
+        syncProgressId = this.mediaId
+    }
 }
 
 
@@ -321,18 +339,30 @@ fun Int.toLanguage(): String {
 }
 
 @Composable
-fun TTVideoView(modifier: Modifier, playbackController: PlaybackController) {
+fun TTVideoView(modifier: Modifier, playbackController: PlaybackController, isSelected: Boolean) {
+
+
     val context = LocalContext.current
     val videoView = remember {
         VideoView(context).apply {
             selectDisplayView(DisplayView.DISPLAY_VIEW_TYPE_TEXTURE_VIEW)
             displayMode = DisplayModeHelper.DISPLAY_MODE_ASPECT_FIT
             playScene = VolcScene.SCENE_SHORT_VIDEO
-            playbackController.bind(this)
 
             val mediaSource = createVidMediaSource()
+            val volcConfig = VolcConfig().apply {
+                this.enableSubtitle = true
+                this.enableSubtitlePreloadStrategy = true
+                subtitleLanguageIds = listOf(1, 2, 3)
+            }
+            VolcConfig.set(mediaSource, volcConfig)
+
             bindDataSource(mediaSource)
         }
+    }
+
+    var subtitleText by remember {
+        mutableStateOf("")
     }
 
     LifecycleResumeEffect(Unit) {
@@ -340,19 +370,130 @@ fun TTVideoView(modifier: Modifier, playbackController: PlaybackController) {
         onPauseOrDispose {
             videoView.pausePlayback()
         }
+
     }
 
     DisposableEffect(videoView) {
+
+        val eventListener = Dispatcher.EventListener { event ->
+            when (event.code()) {
+                PlayerEvent.State.PREPARING -> {
+                    videoView.player()?.setSubtitleEnabled(true)
+                }
+
+                PlayerEvent.State.PREPARED -> {
+
+                }
+
+                PlayerEvent.Info.PROGRESS_UPDATE -> {
+                    val progressInfo = event.cast(InfoProgressUpdate::class.java)
+                }
+
+                PlayerEvent.Info.SUBTITLE_TEXT_UPDATE -> {
+                    //字幕变化回调
+                    val e = event.cast(InfoSubtitleTextUpdate::class.java)
+                    subtitleText = e.subtitleText.text ?: ""
+                    Log.e("字幕变化回调","字幕变化回调==${e.subtitleText.text}")
+                }
+
+                PlayerEvent.Info.SUBTITLE_LIST_INFO_READY -> {
+                    //字幕信息获取成功
+                    videoView.player()?.let { player ->
+                        val subtitles = player.subtitles
+                        subtitles?.find { it.subtitleId == 1 }?.let { subtitle ->
+                            player.selectSubtitle(subtitle)
+                        }
+
+                    }
+
+
+                }
+
+
+            }
+        }
+        playbackController.addPlaybackListener(eventListener)
         onDispose {
             videoView.stopPlayback()
+            playbackController.removePlaybackListener(eventListener)
         }
     }
 
+    LaunchedEffect(isSelected) {
+        if(isSelected){
+            playbackController.bind(videoView)
+            playbackController.startPlayback()
+        }else{
+            playbackController.pausePlayback()
+        }
+    }
 
-    AndroidView(
-        modifier = Modifier
-            .fillMaxSize()
-            .then(modifier), factory = { _ ->
-            videoView
-        })
+    Box {
+        AndroidView(
+            modifier = Modifier
+                .fillMaxSize()
+                .then(modifier), factory = { _ ->
+                videoView
+            })
+        Text(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 100.dp, start = 16.dp, end = 16.dp),
+            text = subtitleText,
+            textAlign = TextAlign.Center,
+            color = Color.Red
+        )
+
+        TTVodDebugTool()
+    }
+
+}
+
+@Composable
+fun TTVodDebugTool(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val frameLayout = remember {
+        FrameLayout(context)
+    }
+
+    AndroidView(modifier = modifier.fillMaxSize(), factory = {
+        frameLayout
+    })
+
+    DisposableEffect(frameLayout) {
+
+        VolcDebugTools.setContainerView(frameLayout)
+        onDispose {
+            VolcDebugTools.release()
+        }
+    }
+
+}
+
+
+@Composable
+fun VideoList(playbackController: PlaybackController) {
+    val state = rememberPagerState { 10 }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        VerticalPager(
+            modifier = Modifier.fillMaxSize(),
+            state = state,
+            beyondViewportPageCount = 1
+        ) { index ->
+            Box {
+                TTVideoView(
+                    modifier = Modifier.fillMaxSize(),
+                    playbackController = PlaybackController(),
+                    isSelected = index==state.settledPage
+                )
+                TTVodDebugTool(modifier = Modifier.fillMaxSize())
+            }
+
+        }
+
+
+
+    }
 }
